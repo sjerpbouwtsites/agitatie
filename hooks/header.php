2a2578ecc4d9436f4aa89fb476c09d54fd4f38e7
Sjerp van Wouden <dev@sjerpbouwtsites.nl>
2021-11-26 21:29:13 +0100

    voorpagina correcties

--- hooks/header.php
+++ hooks/header.php
@@ -19,8 +19,9 @@ if (!function_exists('ag_kop_rechts')) : function ag_kop_rechts()
 		<div class='stek-kop-rechts'>
 			<?php ag_kop_menu_ctrl('horizontaal met-sub menu'); ?>
 			<a id='mobiele-menu-schakel' href='#' class='schakel kopmenu-mobiel' data-toon='.stek-kop-rechts .openklap-menu-container'>
-				Menu <?php ag_mdi('menu', true);
-							ag_mdi('close', true); ?>
+				<span class='menu-menu-tekst'>Menu</span>
+				<?php ag_mdi('menu', true);
+				ag_mdi('close', true); ?>
 			</a>
 			<?php get_template_part('sja/meertalig'); ?>
 		</div>
