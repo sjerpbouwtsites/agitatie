b33ced31197629e87d209c8a67693cad66094406
Sjerp van Wouden <ik@sjerpvanwouden.nl>
2023-12-17 18:03:43 +0100

    dev

--- hooks/agenda.php
+++ hooks/agenda.php
@@ -29,10 +29,10 @@ if (!function_exists('ag_agenda_plek_adres')) :
         }
 
         foreach ($plekken as $p) {
-            $straat = get_field('straat', 'plek_' . $p->term_id);
-            $huisnummer = get_field('huisnummer', 'plek_' . $p->term_id);
-            $postcode = get_field('postcode', 'plek_' . $p->term_id);
-            $stad = get_field('stad', 'plek_' . $p->term_id);
+            $straat = get_field('straat', 'locatie_' . $p->term_id);
+            $huisnummer = get_field('huisnummer', 'locatie_' . $p->term_id);
+            $postcode = get_field('postcode', 'locatie_' . $p->term_id);
+            $stad = get_field('stad', 'locatie_' . $p->term_id);
             $tijd = get_field('datum');
 
             echo "<address class='agenda-address'>
