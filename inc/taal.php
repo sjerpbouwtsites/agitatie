75dce4f9ff5d16fb31611a097a0db8ff50e58607
Sjerp van Wouden <ik@sjerpvanwouden.nl>
2024-03-17 10:00:45 +0100

    e

--- inc/taal.php
+++ inc/taal.php
@@ -109,7 +109,7 @@ function registreer_strengen()
       reg_streng_h('agenda_events', 'events', 'agitatie-agenda'),
       reg_streng_h('agenda_events_archief', 'events archief', 'agitatie-agenda'),
       reg_streng_h('agenda_events_online', 'Locatie wordt gemaild', 'agitatie-agenda'),
-      reg_streng_h('agenda_events_prive', 'Privé event', 'agitatie-agenda'),
+      reg_streng_h('agenda_events_prive', 'Privé locatie', 'agitatie-agenda'),
       reg_streng_h('agenda_gepland', 'gepland', 'agitatie-agenda'),
       reg_streng_h('agenda_bekijk_volledige_agenda', 'volledige agenda', 'agitatie-agenda'),
       reg_streng_h('agenda_filter_events_met', 'Filter events op', 'agitatie-agenda'),
